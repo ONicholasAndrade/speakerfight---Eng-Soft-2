994a2077d7ec45df7ebe41f55653bff8ee9f9a8e
Thiago Ker <thiagker1@gmail.com>
2026-07-17 21:12:46 -0300

    ci: configurar pipeline para testes de aceitacao

--- acceptance_tests/test_proposal_window.py
+++ acceptance_tests/test_proposal_window.py
@@ -1,30 +1,69 @@
 from datetime import timedelta
 
-from django.test import TestCase
 from django.contrib.auth.models import User
-from django.core.urlresolvers import reverse
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from django.utils.timezone import now
 
 from model_mommy import mommy
 
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+
 from deck.models import Event, Proposal
 
 
-class ProposalWindowAcceptanceTests(TestCase):
+class ProposalWindowAcceptanceTests(StaticLiveServerTestCase):
 
     def setUp(self):
+
+        options = Options()
+        options.add_argument('--headless')
+        options.add_argument('--no-sandbox')
+        options.add_argument('--disable-dev-shm-usage')
+
+        self.browser = webdriver.Chrome(
+            options=options
+        )
+
         self.user = User.objects.create_user(
             username='thiago',
             password='123456'
         )
 
-        self.client.login(
-            username='thiago',
-            password='123456'
+
+    def tearDown(self):
+        self.browser.quit()
+
+
+    def login(self):
+
+        self.browser.get(
+            self.live_server_url +
+            '/accounts/login/'
         )
 
+        self.browser.find_element(
+            By.NAME,
+            'login'
+        ).send_keys(
+            'thiago'
+        )
 
-    def test_user_cannot_access_proposal_before_opening_date(self):
+        self.browser.find_element(
+            By.NAME,
+            'password'
+        ).send_keys(
+            '123456'
+        )
+
+        self.browser.find_element(
+            By.CSS_SELECTOR,
+            'button[type="submit"]'
+        ).click()
+
+
+    def test_before_opening_date_blocks_access(self):
 
         event = mommy.make(
             Event,
@@ -32,22 +71,20 @@ class ProposalWindowAcceptanceTests(TestCase):
             closing_date=now() + timedelta(days=5)
         )
 
-        response = self.client.get(
-            reverse(
-                'create_event_proposal',
-                args=[event.slug]
-            )
+        self.login()
+
+        self.browser.get(
+            self.live_server_url +
+            '/events/{}/proposals/create/'.format(event.slug)
         )
 
-        self.assertEqual(response.status_code, 302)
-
-        self.assertEqual(
-            Proposal.objects.count(),
-            0
+        self.assertIn(
+            "This Event doesn't accept Proposals yet.",
+            self.browser.page_source
         )
 
 
-    def test_user_can_create_proposal_after_opening_date(self):
+    def test_create_proposal_after_opening_date(self):
 
         event = mommy.make(
             Event,
@@ -55,17 +92,49 @@ class ProposalWindowAcceptanceTests(TestCase):
             closing_date=now() + timedelta(days=5)
         )
 
-        response = self.client.get(
-            reverse(
-                'create_event_proposal',
-                args=[event.slug]
-            )
+        self.login()
+
+        self.browser.get(
+            self.live_server_url +
+            '/events/{}/proposals/create/'.format(event.slug)
         )
 
-        self.assertEqual(response.status_code, 200)
+
+        self.browser.find_element(
+            By.NAME,
+            'title'
+        ).send_keys(
+            'Proposta Selenium'
+        )
 
 
-    def test_user_cannot_create_proposal_after_closing_date(self):
+        self.browser.find_element(
+            By.NAME,
+            'description'
+        ).send_keys(
+            'Descricao criada pelo teste de aceitacao'
+        )
+
+
+        self.browser.find_element(
+            By.CSS_SELECTOR,
+            'button[type="submit"]'
+        ).click()
+
+
+        self.assertIn(
+            'Proposal created.',
+            self.browser.page_source
+        )
+
+
+        self.assertEqual(
+            Proposal.objects.count(),
+            1
+        )
+
+
+    def test_after_closing_date_blocks_access(self):
 
         event = mommy.make(
             Event,
@@ -73,11 +142,14 @@ class ProposalWindowAcceptanceTests(TestCase):
             closing_date=now() - timedelta(days=1)
         )
 
-        response = self.client.get(
-            reverse(
-                'create_event_proposal',
-                args=[event.slug]
-            )
+        self.login()
+
+        self.browser.get(
+            self.live_server_url +
+            '/events/{}/proposals/create/'.format(event.slug)
         )
 
-        self.assertEqual(response.status_code, 302)
+        self.assertIn(
+            "This Event doesn't accept Proposals anymore.",
+            self.browser.page_source
+        )
